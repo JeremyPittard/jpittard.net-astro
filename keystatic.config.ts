6e9d2fa2138797ad75fe63bc3829fb05c40b89c7
Jeremy Pittard <jeremy@jpittard.net>
2024-04-01 10:50:52 +0800

    github auth login

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,11 @@ import { config, fields, collection } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: "local",
+    kind: "github",
+    repo: {
+      owner: "JeremyPittard",
+      name: "jpittard.net-astro",
+    },
   },
   collections: {
     posts: collection({
