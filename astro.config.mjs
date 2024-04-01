e709c8ae4f6d1f938b62b5d2d1fd0a3391feb732
Jeremy Pittard <jeremy@jpittard.net>
2024-04-01 10:44:52 +0800

    config local env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import db from "@astrojs/db";
-
 import netlify from "@astrojs/netlify";
+
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()],
+  integrations: [db(), react(), markdoc(), keystatic()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
---
+++ keystatic.config.ts
@@ -0,0 +1,25 @@
+import { config, fields, collection } from "@keystatic/core";
+
+export default config({
+  storage: {
+    kind: "local",
+  },
+  collections: {
+    posts: collection({
+      label: "Thoughts",
+      slugField: "title",
+      path: "src/content/thoughts/*",
+      format: { contentField: "content" },
+      schema: {
+        title: fields.slug({ name: { label: "Title" } }),
+        content: fields.document({
+          label: "Content",
+          formatting: true,
+          dividers: true,
+          links: true,
+          images: true,
+        }),
+      },
+    }),
+  },
+});
